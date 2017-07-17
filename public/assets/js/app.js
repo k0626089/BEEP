7b8cfd4a207ea0ec1d3d999316119dc81dfe1d09
k0626089 <k0626089@gmail.com>
2017-07-17 17:16:30 +0900

    add block add function

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,11 +1,39 @@
-/*
-function searchFunction() {
-    var table = document.getElementById("TableData");
-    var tr = table.getElementsByTagName("tr");
-    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
-        tr.show().filter(function() {
-            var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-            return !~text.indexOf(val);
-        }).hide();
+function searchFunction(octav, note, title, artist, sharp) {
+    "use strict";
+    var outer_div = document.createElement("div"),
+        octave_div = document.createElement("div"),
+        main_div = document.createElement("div"),
+        
+        title_p = document.createElement("p"),
+        artist_p = document.createElement("p"),
+        sharp_p = document.createElement("p"),
+        
+        sharpnode = document.createTextNode("#"),
+        octnode = document.createTextNode(note),
+        titlenode = document.createTextNode(title),
+        artistnode = document.createTextNode(artist),
+        
+        oct_class = "circle oct_".concat(octav),
+        
+        br = document.createElement("br");
+    
+    octave_div.appendChild(octnode);
+    
+    sharp_p.appendChild(sharpnode);
+    title_p.appendChild(titlenode);
+    artist_p.appendChild(artistnode);
+    
+    octave_div.appendChild(sharp_p);
+    main_div.appendChild(title_p);
+    main_div.appendChild(artist_p);
+        
+    octave_div.className = oct_class;
+    sharp_p.className = "sharp";
+    title_p.className = "title";
+    artist_p.className = "artist";
+    
+    outer_div.appendChild(octave_div);
+    outer_div.appendChild(main_div);
+    outer_div.appendChild(br);
+    document.getElementById("blocklist").appendChild(outer_div);
 }
-*/
