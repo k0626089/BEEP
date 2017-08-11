7769ab819f09d02c192fa8d0c9d7b35807eb97d0
k0626089 <k0626089@gmail.com>
2017-08-11 18:12:01 +0900

    improve search mechanism

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -89,30 +89,43 @@ function searchBlocks() {
     ref.on('value', function (snapshot) {
         snapshot.forEach(function (childSnapshot) {
             var key = childSnapshot.key,
-			
 				octave = childSnapshot.val().octave,
                 note = childSnapshot.val().note,
                 title = childSnapshot.val().title,
                 artist = childSnapshot.val().artist,
                 sharp = childSnapshot.val().sharp,
-				view = childSnapshot.val().view_count;
+                view = childSnapshot.val().view_count;
+            
+            var k_artist = artist.split("(")[0],
+                e_artist = artist.split("(")[1];
+                e_artist = artist.split(")")[0];
 
             var t = title.toUpperCase().replace(/\s/g, ''),
-                a = artist.toUpperCase().replace(/\s/g, ''),
+                ka = k_artist.toUpperCase().replace(/\s/g, ''),
+                ea = e_artist.toUpperCase().replace(/\s/g, ''),
                 k = keyword.toUpperCase().replace(/\s/g, ''),
                 o = octave.toString(),
-                s1 = a.concat(o, "옥", note, o, "옥타브", note),
-                s2 = t.concat(o, "옥", note, o, "옥타브", note),
-                s3 = a.concat(t, o, "옥", note, o, "옥타브", note),
-                s4 = t.concat(a, o, "옥", note, o, "옥타브", note),
-                s5 = o.concat("옥", note, a, o, "옥타브", note, a),
-                s6 = o.concat("옥", note, t, o, "옥타브", note, t),
-                s7 = o.concat("옥", note, a, t, o, "옥타브", note, a, t),
-                s8 = o.concat("옥", note, t, a, o, "옥타브", note, t, a),
-                s9 = o.concat("옥", a, o, "옥타브", a),
-                s10 = o.concat("옥", t, o, "옥타브", t),
-                s11 = o.concat("옥", a, t, o, "옥타브", a, t),
-                s12 = o.concat("옥", t, a, o, "옥타브", t, a);
+                s1 = ka.concat(o, "옥", note, o, "옥타브", note),
+                s2 = ea.concat(o, "옥", note, o, "옥타브", note),
+                s3 = t.concat(o, "옥", note, o, "옥타브", note),
+                s4 = ka.concat(t, o, "옥", note, o, "옥타브", note),
+                s5 = ea.concat(t, o, "옥", note, o, "옥타브", note),
+                s6 = t.concat(ka, o, "옥", note, o, "옥타브", note),
+                s7 = t.concat(ea, o, "옥", note, o, "옥타브", note),
+                s8 = o.concat("옥", note, ka, o, "옥타브", note, ka),
+                s9 = o.concat("옥", note, ea, o, "옥타브", note, ea),
+                s10 = o.concat("옥", note, t, o, "옥타브", note, t),
+                s11 = o.concat("옥", note, ka, t, o, "옥타브", note, ka, t),
+                s12 = o.concat("옥", note, ea, t, o, "옥타브", note, ea, t),
+                s13 = o.concat("옥", note, t, ka, o, "옥타브", note, t, ka),
+                s14 = o.concat("옥", note, t, ea, o, "옥타브", note, t, ea),
+                s15 = o.concat("옥", ka, o, "옥타브", ka),
+                s16 = o.concat("옥", ea, o, "옥타브", ea),
+                s17 = o.concat("옥", t, o, "옥타브", t),
+                s18 = o.concat("옥", ka, t, o, "옥타브", ka, t),
+                s19 = o.concat("옥", ea, t, o, "옥타브", ea, t),
+                s20 = o.concat("옥", t, ka, o, "옥타브", t, ka),
+                s21 = o.concat("옥", t, ea, o, "옥타브", t, ea);
             
             if (i >= page_item) { 
 				more_button();
@@ -123,7 +136,12 @@ function searchBlocks() {
                     || s5.includes(k) || s6.includes(k)
                     || s7.includes(k) || s8.includes(k)
                     || s9.includes(k) || s10.includes(k)
-                    || s11.includes(k) || s12.includes(k)) {
+                    || s11.includes(k) || s12.includes(k)
+                    || s13.includes(k) || s14.includes(k)
+                    || s15.includes(k) || s16.includes(k)
+                    || s17.includes(k) || s18.includes(k)
+                    || s19.includes(k) || s20.includes(k)
+                    || s21.includes(k)) {
                     
                     addBlock(octave, note, title, artist, sharp);
 //					firebase.database().ref().child('/songs/' + key + '/view_count').set(view + 1);
